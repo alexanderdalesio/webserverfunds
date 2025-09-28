b40e84ea312f1aa80c4a033f817c7ac7c9981cad
Alexander D'Alesio <alexander.dalesio@yahoo.com>
2025-09-28 16:01:02 -0400

    sanitizing form response data and improving readability

--- site/page.php
+++ site/page.php
@@ -1,4 +1,6 @@
 <p><?= $_SERVER["HTTP_USER_AGENT"] ?></p>
 <br><br>
-<p>GET: <?= var_dump($_GET) ?></p>
-<p>POST: <?= var_dump($_POST) ?></p>
+<p>Your favorite puppy breed is: <?= htmlspecialchars($_POST['breed1']) ?></p>
+<p>Followed by: <?= htmlspecialchars($_POST['breed2']) ?></p>
+<p>And lastly: <?= htmlspecialchars($_POST['breed3']) ?></p>
+<p>You successfully confirmed your submission by entering: <?= htmlspecialchars($_POST['confirm']) ?></p>
