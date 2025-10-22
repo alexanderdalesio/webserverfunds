ad73cb059c18cb25ff2f98efa44b2c3089e42b8a
Alexander D'Alesio <alexander.dalesio@yahoo.com>
2025-10-22 12:09:29 -0400

    Update page.php

--- site/page.php
+++ site/page.php
@@ -17,7 +17,7 @@ $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "✅ Connected 333333<br>";
+echo "✅ Connected successfully<br>";
 
 $breed1 = htmlspecialchars($_POST['breed1']);
 $breed2 = htmlspecialchars($_POST['breed2']);
