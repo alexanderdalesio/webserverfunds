a10fc455bbf3250ece870b282c623d35e644e98a
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-11-19 14:00:54 -0500

    Update script.js

--- site/script.js
+++ site/script.js
@@ -12,13 +12,13 @@ console.log('onButton:', onButton);
 console.log('offButton:', offButton);
 console.log('toggleButton:', toggleButton);
 
-// Start with BG1
+// Start BG1
 let bgState = 1;
 bg.style.backgroundImage = "url('media/paws.png')";
 buttonText.innerHTML = "Change Background";
 
-// Toggle backgrounds on click
-button.onclick = function() {
+// Toggle backgrounds
+button.onclick = function () {
     if (bgState === 1) {
         bg.style.backgroundImage = "url('media/bones.png')";
         buttonText.innerHTML = "Background 2";
@@ -30,45 +30,23 @@ button.onclick = function() {
     }
 };
 
-// Hover effect for main button
-button.onmouseover = function() {
-    console.log('HOVER ON');
-    button.style.backgroundColor = '#4f6184ff';
-};
-button.onmouseout = function() {
-    button.style.backgroundColor = '';
-};
+// Hover: main button
+button.onmouseover = () => button.style.backgroundColor = '#4f6184ff';
+button.onmouseout  = () => button.style.backgroundColor = '';
 
-// Hover effect for ON button
-onButton.onmouseover = function() {
-    console.log('HOVER ON');
-    onButton.style.backgroundColor = '#4f6184ff';
-};
-onButton.onmouseout = function() {
-    onButton.style.backgroundColor = '';
-};
+// Hover: GPIO buttons
+onButton.onmouseover = () => onButton.style.backgroundColor = '#4f6184ff';
+onButton.onmouseout  = () => onButton.style.backgroundColor = '';
 
-// Hover effect for OFF button
-offButton.onmouseover = function() {
-    console.log('HOVER ON');
-    offButton.style.backgroundColor = '#4f6184ff';
-};
-offButton.onmouseout = function() {
-    offButton.style.backgroundColor = '';
-};
+offButton.onmouseover = () => offButton.style.backgroundColor = '#4f6184ff';
+offButton.onmouseout  = () => offButton.style.backgroundColor = '';
 
-// Hover effect for TOGGLE button
-toggleButton.onmouseover = function() {
-    console.log('HOVER ON');
-    toggleButton.style.backgroundColor = '#4f6184ff';
-};
-toggleButton.onmouseout = function() {
-    toggleButton.style.backgroundColor = '';
-};
+toggleButton.onmouseover = () => toggleButton.style.backgroundColor = '#4f6184ff';
+toggleButton.onmouseout  = () => toggleButton.style.backgroundColor = '';
 
-// Required field alert logic
+// Required field (ONLY if it exists on the page)
 if (requiredField) {
-    requiredField.onblur = function() {
+    requiredField.onblur = function () {
         if (!requiredField.value) {
             alert('Please fill out the confirmation field with PUPPY.');
         }
