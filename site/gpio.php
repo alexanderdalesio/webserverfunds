a27d68b294388bfad968a40a5bb1a214a80ad6dd
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-11-19 14:11:58 -0500

    Update gpio.php

--- site/gpio.php
+++ site/gpio.php
@@ -6,29 +6,24 @@ $database = "puppiesDB";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 
-if (!isset($_GET['action'])) {
-    http_response_code(400);
-    echo "no action";
-    exit;
-}
-
-$action = $_GET['action'];
+$action = $_GET['action'] ?? null;
 
 if ($action === "on") {
-    // example GPIO or shell command
     shell_exec("gpio write 7 1");
     echo "GPIO turned ON";
 }
+
 else if ($action === "off") {
     shell_exec("gpio write 7 0");
     echo "GPIO turned OFF";
 }
+
 else if ($action === "toggle") {
     shell_exec("gpio toggle 7");
     echo "GPIO toggled";
 }
-else {
-    http_response_code(400);
-    echo "invalid action";
+
+else { 
+    echo "no action"; 
 }
 ?>
