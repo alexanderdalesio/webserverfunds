75b6592fc521de29c11610391fdee42383d4c579
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-11-19 15:28:27 -0500

    Update gpio-ctrl.php

--- site/gpio-ctrl.php
+++ site/gpio-ctrl.php
@@ -63,17 +63,17 @@
             <table class="sensor-values">
                 <tr>
                     <td>
-                        <p class="sensor-value"><b>Temperature:</b></p>
+                        <p class="sensor-value"><b>Temperature</b>:</p>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p class="sensor-value"><b>Humidity:</b></p>
+                        <p class="sensor-value"><b>Humidit</b>:</p>
                     </td>
                 </tr>
                 <tr>    
                     <td>
-                        <p class="sensor-value"><b>Pressure:</b></p>
+                        <p class="sensor-value"><b>Pressure</b>:</p>
                     </td>
                 </tr>
             </table>
