28895369f55ae67cccac65fe653f320c6075f39d
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-12-19 11:34:13 -0500

    css

--- phishing/index.php
+++ phishing/index.php
@@ -19,22 +19,20 @@ $conn = mysqli_connect($server, $username, $password, $database);
         <title>Google Search</title>
     </head>
     <body>
-        <div class="content">
-            <h1>
-                <span class="blue">G</span>
-                <span class="red">o</span>
-                <span class="yellow">o</span>
-                <span class="blue">g</span>
-                <span class="green">l</span>
-                <span class="red">e</span>
-            </h1>
-            
-            <div class="search-box">
-                <form action="/search.php" method="post">
-                    <input type="text" id="query" name="query">
-                    <input type="submit" value="Search">
-                </form>
-            </div>
+        <h1>
+            <span class="blue">G</span>
+            <span class="red">o</span>
+            <span class="yellow">o</span>
+            <span class="blue">g</span>
+            <span class="green">l</span>
+            <span class="red">e</span>
+        </h1>
+        
+        <div class="search-box">
+            <form action="/search.php" method="post">
+                <input type="text" id="query" name="query">
+                <input type="submit" value="Search">
+            </form>
         </div>
     </body>
 </html>
