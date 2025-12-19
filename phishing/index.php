e444aa6f7c03a4daa65d655bec3bc056ad381c87
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-12-19 10:42:16 -0500

    Update index.php

--- phishing/index.php
+++ phishing/index.php
@@ -1,4 +1,16 @@
 <!-- Connect to the database -->
+<?php
+$server = "alexpi.local";
+$username = "php";
+$password = "password";
+$database = "phishingDB";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "✅ Connected successfully<br>";
+?>
 
 <!DOCTYPE html>
 <html>
@@ -10,4 +22,3 @@
         <p>Search</p>
     </body>
 </html>
-
