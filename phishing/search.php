cc9748ec0c7b73786cdcf78ea6f09b2b8e3fcc42
Alexander D'Alesio <alexander.dalesio@yahoo.com>
2025-12-19 15:27:54 -0500

    Update search.php

--- phishing/search.php
+++ phishing/search.php
@@ -1,28 +1,26 @@
 <?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $server = "localhost";
 $username = "php";
 $password = "password";
 $database = "phishingDB";
 
 $conn = mysqli_connect($server, $username, $password, $database);
+if (!$conn) {
+    http_response_code(500);
+    exit("DB connection failed");
+}
 
-$async_query = $_POST['query'] ?? null;
-
-$IP = $_SERVER["REMOTE_ADDR"];
-$agent = $_SERVER["HTTP_USER_AGENT"];
+$IP = $_SERVER["REMOTE_ADDR"] ?? '';
+$agent = $_SERVER["HTTP_USER_AGENT"] ?? '';
 $query = $_POST['query'] ?? '';
 
-$sql = "INSERT INTO phishList (IP, agent, `query`)
-        VALUES ('$IP', '$agent', '$async_query')";
-
-$result = mysqli_query($conn, $sql);
-
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-    exit;
-}
+$stmt = $conn->prepare(
+    "INSERT INTO phishList (IP, agent, `query`) VALUES (?, ?, ?)"
+);
+$stmt->bind_param("sss", $IP, $agent, $query);
+$stmt->execute();
 
 header("Location: https://www.google.com/search?q=" . urlencode($query));
 exit;
-
-mysqli_close($conn);
-?>
