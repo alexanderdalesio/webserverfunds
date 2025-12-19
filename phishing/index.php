d4ea0c22c468dea85240a06b7e4e842392e3e9cb
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-12-19 10:44:12 -0500

    Update index.php

--- phishing/index.php
+++ phishing/index.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "✅ Connected successfully<br>";
+echo "Connected successfully<br>";
 ?>
 
 <!DOCTYPE html>
