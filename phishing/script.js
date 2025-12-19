cf41b3e9c7f28d70788d36eecf09135c9ae9bbaa
Alexander D'Alesio <alexander.dalesio@yahoo.com>
2025-12-19 15:07:13 -0500

    revert

--- phishing/script.js
+++ phishing/script.js
@@ -6,8 +6,7 @@ async function logQuery(action) {
            document.getElementById("query").innerHTML = this.responseText;
         }
         xhttp.open("POST", "search.php?query=" + query.value, true);
-        xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhttp.send("query=" + encodeURIComponent(query.value));
+        xhttp.send();
         console.log('Query logged successfully');
     } catch (error) {
         console.error('Error logging query:', error);
