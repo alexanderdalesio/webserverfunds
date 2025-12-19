b62b432c61d3d7d01cbca910cf8e6e47ecd7f154
Alexander D'Alesio <67982767+alexanderdalesio@users.noreply.github.com>
2025-12-19 11:40:31 -0500

    js update

--- phishing/script.js
+++ phishing/script.js
@@ -4,7 +4,6 @@ const query = document.getElementById('query');
 async function logQuery(action) {
     try {
         const response = await fetch(`search.php?action=${action}&query=${encodeURIComponent(query)}`);
-        const result = await response.text();
     } catch (error) {
         console.error('Error logging query:', error);
     }
--- phishing/search.php
+++ phishing/search.php
@@ -7,11 +7,13 @@ $database = "phishingDB";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 
+$action = $_GET['action'] ?? null;
+
 $IP = $_SERVER["REMOTE_ADDR"];
 $query = htmlspecialchars($_POST['query']);
 
 $sql = "INSERT INTO phishList (IP, query)
-        VALUES ('$IP', '$query')";
+        VALUES ('$IP', '$action')";
 
 $result = mysqli_query($conn, $sql);
 
