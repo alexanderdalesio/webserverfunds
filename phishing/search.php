128f0367712945582f3c76bf4f99c59353cebfe9
Alexander D'Alesio <alexander.dalesio@yahoo.com>
2025-12-19 15:14:41 -0500

    Update search.php

--- phishing/search.php
+++ phishing/search.php
@@ -1,4 +1,3 @@
-<!-- Database (phishingDB) -->
 <?php
 $server = "localhost";
 $username = "php";
@@ -6,18 +5,14 @@ $password = "password";
 $database = "phishingDB";
 
 $conn = mysqli_connect($server, $username, $password, $database);
-if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
-                echo "✅ Connected successfully<br>";
 
 $async_query = $_POST['query'] ?? null;
 
 $IP = $_SERVER["REMOTE_ADDR"];
 $agent = $_SERVER["HTTP_USER_AGENT"];
-$query = htmlspecialchars($_POST['query']);
+$query = $_POST['query'] ?? '';
 
-$sql = "INSERT INTO phishList (IP, agent, query)
+$sql = "INSERT INTO phishList (IP, agent, `query`)
         VALUES ('$IP', '$agent', '$async_query')";
 
 $result = mysqli_query($conn, $sql);
